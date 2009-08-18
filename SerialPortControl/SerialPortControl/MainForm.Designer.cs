41cad5299cd1120e850b00fc33301763d6668008
daniel <daniel@c3d2fdb0-54ce-6b4a-ae4e-5b7e9bd45ceb>
2009-08-18 22:35:20 +0000

    work on main form
    git-svn-id: https://Daniel-G50vt/svn/SerialPortControl/trunk@7 c3d2fdb0-54ce-6b4a-ae4e-5b7e9bd45ceb

--- SerialPortControl/SerialPortControl/MainForm.Designer.cs
+++ SerialPortControl/SerialPortControl/MainForm.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.commandsTabPage = new System.Windows.Forms.TabPage();
+            this.configurationTabPage = new System.Windows.Forms.TabPage();
+            this.configurationTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.configurationTabPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.okButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cancelButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tabControl, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(386, 466);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tabControl
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.tabControl, 2);
+            this.tabControl.Controls.Add(this.commandsTabPage);
+            this.tabControl.Controls.Add(this.configurationTabPage);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(3, 3);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(380, 430);
+            this.tabControl.TabIndex = 1;
+            // 
+            // commandsTabPage
+            // 
+            this.commandsTabPage.Location = new System.Drawing.Point(4, 26);
+            this.commandsTabPage.Name = "commandsTabPage";
+            this.commandsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.commandsTabPage.Size = new System.Drawing.Size(387, 167);
+            this.commandsTabPage.TabIndex = 0;
+            this.commandsTabPage.Text = "Commands";
+            this.commandsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // configurationTabPage
+            // 
+            this.configurationTabPage.Controls.Add(this.configurationTableLayoutPanel);
+            this.configurationTabPage.Location = new System.Drawing.Point(4, 24);
+            this.configurationTabPage.Name = "configurationTabPage";
+            this.configurationTabPage.Size = new System.Drawing.Size(372, 402);
+            this.configurationTabPage.TabIndex = 1;
+            this.configurationTabPage.Text = "Configuration";
+            this.configurationTabPage.UseVisualStyleBackColor = true;
+            // 
+            // configurationTableLayoutPanel
+            // 
+            this.configurationTableLayoutPanel.ColumnCount = 2;
+            this.configurationTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.configurationTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.configurationTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.configurationTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.configurationTableLayoutPanel.Name = "configurationTableLayoutPanel";
+            this.configurationTableLayoutPanel.RowCount = 1;
+            this.configurationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.configurationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.configurationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.configurationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.configurationTableLayoutPanel.Size = new System.Drawing.Size(372, 402);
+            this.configurationTableLayoutPanel.TabIndex = 0;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(308, 439);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 24);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.Location = new System.Drawing.Point(227, 439);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 24);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(394, 474);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serial Port Control";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tabControl.ResumeLayout(false);
+            this.configurationTabPage.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage commandsTabPage;
+        private System.Windows.Forms.TabPage configurationTabPage;
+        private System.Windows.Forms.TableLayoutPanel configurationTableLayoutPanel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+
+
     }
 }
 
