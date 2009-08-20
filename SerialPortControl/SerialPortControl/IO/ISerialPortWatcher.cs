f2bb5d8597a28e611914aee4dc3c835a789807e2
daniel <daniel@c3d2fdb0-54ce-6b4a-ae4e-5b7e9bd45ceb>
2009-08-20 12:16:24 +0000

    added ISerialPortWatcher interface
    git-svn-id: https://Daniel-G50vt/svn/SerialPortControl/trunk@23 c3d2fdb0-54ce-6b4a-ae4e-5b7e9bd45ceb

---
+++ SerialPortControl/SerialPortControl/IO/ISerialPortWatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SerialPortControl.Model;
+
+namespace SerialPortControl.IO
+{
+    public interface ISerialPortWatcher
+    {
+        event EventHandler<ReceivedDataEventArgs> OnReceivedData;
+
+        SerialPortSettings SerialPort { set; }
+    }
+
+    public class ReceivedDataEventArgs : EventArgs
+    {
+        public ReceivedDataEventArgs(string data)
+        {
+            Data = data;
+        }
+        public string Data { get; private set; }
+    }
+}
